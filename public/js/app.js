b42e8504ae5b89d8b461013725d9cd9bd35ca350
Henrique Pereira <62767337+halvespereira@users.noreply.github.com>
2021-04-05 11:49:28 -0700

    changes

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,7 @@ const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
+//event listener for submitting form
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
